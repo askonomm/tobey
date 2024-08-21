f8ac5a906d80d236d7e610f13681649386832e7f
Asko Nõmm <asko@nth.ee>
2024-08-22 02:23:17 +0300

    bump

--- main.cpp
+++ main.cpp
@@ -4,10 +4,7 @@
 #include "src/tobey.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     tobey::run(std::filesystem::current_path().string());
-    setvbuf (stdout, nullptr, _IONBF, 0);
 
-    printf("Hello, World!\n");
-    return 1;
+    return 0;
 }
--- src/tobey.cpp
+++ src/tobey.cpp
@@ -15,7 +15,26 @@ namespace tobey {
 
         for (const auto &entry : std::filesystem::directory_iterator(directory)) {
             if (entry.is_regular_file() && entry.path().extension() == ".md") {
-                files.push_back(entry.path().string());
+                const auto relative_path = std::filesystem::relative(entry.path(), directory);
+                files.push_back(relative_path.string());
+            }
+        }
+
+        return files;
+    }
+
+    std::vector<std::string> get_layout_files(const std::string &directory) {
+        std::vector<std::string> files;
+
+        // if directory does not exist, attempt creating it
+        if (!std::filesystem::exists(directory)) {
+            std::filesystem::create_directory(directory);
+        }
+
+        for (const auto &entry : std::filesystem::directory_iterator(directory)) {
+            if (entry.is_regular_file() && entry.path().extension() == ".html") {
+                const auto relative_path = std::filesystem::relative(entry.path(), directory);
+                files.push_back(relative_path.string());
             }
         }
 
@@ -23,15 +42,8 @@ namespace tobey {
     }
 
     void run(const std::string& root_dir) {
-        std::cout << "Hello, Tobey!" << std::endl;
-        std::cout << "Root directory: " << root_dir << std::endl;
-
         const auto dir_separator = std::string(1, std::filesystem::path::preferred_separator);
-        const auto content_dir = root_dir + dir_separator + "content";
-        std::vector<std::string> files = get_content_files(content_dir);
-
-        for (const auto &file : files) {
-            std::cout << file << std::endl;
-        }
+        const auto content_files = get_content_files(root_dir + dir_separator + "content");
+        const auto layout_files = get_layout_files(root_dir + dir_separator + "layouts");
     }
 }

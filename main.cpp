9b678dd4d5fa7e556873162e2fe5621f27ad3edb
Asko Nõmm <asko@nth.ee>
2024-08-25 19:10:53 +0300

    Close #10

--- main.cpp
+++ main.cpp
@@ -3,13 +3,12 @@
 #include "src/tobey.hpp"
 
 int main(int argc, char * argv[]) {
+    tobey::run(std::filesystem::current_path().string());
+
     if (argc > 1 && std::string(argv[1]) == "--watch")
     {
-        tobey::run(std::filesystem::current_path().string());
         tobey::watch(std::filesystem::current_path().string());
     }
-
-    tobey::run(std::filesystem::current_path().string());
 
     return 0;
 }

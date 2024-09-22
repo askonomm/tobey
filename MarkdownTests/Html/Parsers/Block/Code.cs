af8b17e2ec1594d17ad3fc19d8aa03abec1d857b
Asko Nõmm <asko@nth.ee>
2024-09-22 22:45:47 +0300

    Add tests for Code block parser

---
+++ MarkdownTests/Html/Parsers/Block/Code.cs
@@ -0,0 +1,23 @@
+﻿namespace MarkdownTests.Html.Parsers.Block;
+
+[TestClass]
+public class Code
+{
+    [TestMethod]
+    public void TestWithLanguageParsing()
+    {
+        var code = new Markdown.Html.Parsers.Block.Code();
+        var result = code.Parse("```csharp\nvar x = 1;\n```");
+
+        Assert.AreEqual("<pre class=\"language-csharp\"><code>var x = 1;</code></pre>", result);
+    }
+
+    [TestMethod]
+    public void TestWithoutLanguageParsing()
+    {
+        var code = new Markdown.Html.Parsers.Block.Code();
+        var result = code.Parse("```\nvar x = 1;\n```");
+
+        Assert.AreEqual("<pre><code>var x = 1;</code></pre>", result);
+    }
+}

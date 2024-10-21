5bb19a1136dc2d2ab65f21e54c5074ba37615c1f
Asko Nõmm <asko@nth.ee>
2024-10-21 10:39:56 +0300

    bump

--- Tobey/Watcher.cs
+++ Tobey/Watcher.cs
@@ -1,81 +1,80 @@
-﻿namespace Tobey
+﻿namespace Tobey;
+
+public class Watcher
 {
-    internal class Watcher
+    private static Timer _debounceTimer;
+    
+    public static void Watch(string path, Action callback)
     {
-        public static void Watch(string path, Action callback)
+        using var watcher = new FileSystemWatcher(path);
+        watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.CreationTime;
+        watcher.IncludeSubdirectories = true;
+        watcher.EnableRaisingEvents = true;
+
+        watcher.Changed += (source, e) => OnChanged(source, e, callback);
+        watcher.Created += (source, e) => OnCreated(source, e, callback);
+        watcher.Deleted += (source, e) => OnDeleted(source, e, callback);
+        watcher.Renamed += (source, e) => OnRenamed(source, e, callback);
+        
+        Console.ReadLine();
+    }
+
+    private static void OnChanged(object _, FileSystemEventArgs e, Action callback)
+    {
+        // Ignore output folder changes
+        if (e.FullPath.Contains("output"))
         {
-            using var watcher = new FileSystemWatcher(path)
-            {
-                NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.CreationTime,
-                IncludeSubdirectories = true,
-                EnableRaisingEvents = true
-            };
-
-            watcher.Changed += (source, e) => OnChanged(source, e, callback);
-            watcher.Created += (source, e) => OnCreated(source, e, callback);
-            watcher.Deleted += (source, e) => OnDeleted(source, e, callback);
-            watcher.Renamed += (source, e) => OnRenamed(source, e, callback);
-
-
-            Console.ReadLine();
+            return;
+        }
+        
+        // Ignore dot files
+        if (e.Name == null || e.Name.StartsWith('.'))
+        {
+            return;
         }
 
-        private static void OnChanged(object _, FileSystemEventArgs e, Action callback)
+        Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
+
+        _debounceTimer?.Dispose();
+        _debounceTimer = new Timer(_ => callback(), null, 500, Timeout.Infinite);
+    }
+
+    private static void OnRenamed(object _, RenamedEventArgs e, Action callback)
+    {
+        // Ignore output folder changes
+        if (e.FullPath.Contains("output"))
         {
-            // Ignore output folder changes
-            if (e.FullPath.Contains("output"))
-            {
-                return;
-            }
-            
-            // Ignore dot files
-            if (e.Name == null || e.Name.StartsWith('.'))
-            {
-                return;
-            }
-
-            Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
-
-            callback();
+            return;
         }
 
-        private static void OnRenamed(object _, RenamedEventArgs e, Action callback)
+        Console.WriteLine($"File: {e.OldFullPath} renamed to {e.FullPath}");
+
+        callback();
+    }
+
+    private static void OnDeleted(object _, FileSystemEventArgs e, Action callback)
+    {
+        // Ignore output folder changes
+        if (e.FullPath.Contains("output"))
         {
-            // Ignore output folder changes
-            if (e.FullPath.Contains("output"))
-            {
-                return;
-            }
-
-            Console.WriteLine($"File: {e.OldFullPath} renamed to {e.FullPath}");
-
-            callback();
+            return;
         }
 
-        private static void OnDeleted(object _, FileSystemEventArgs e, Action callback)
+        Console.WriteLine($"File: {e.FullPath} deleted");
+
+        callback();
+    }
+
+    private static void OnCreated(object _, FileSystemEventArgs e, Action callback)
+    {
+        // Ignore output folder changes
+        if (e.FullPath.Contains("output"))
         {
-            // Ignore output folder changes
-            if (e.FullPath.Contains("output"))
-            {
-                return;
-            }
-
-            Console.WriteLine($"File: {e.FullPath} deleted");
-
-            callback();
+            return;
         }
 
-        private static void OnCreated(object _, FileSystemEventArgs e, Action callback)
-        {
-            // Ignore output folder changes
-            if (e.FullPath.Contains("output"))
-            {
-                return;
-            }
+        Console.WriteLine($"File: {e.FullPath} created");
 
-            Console.WriteLine($"File: {e.FullPath} created");
-
-            callback();
-        }
+        callback();
     }
 }
